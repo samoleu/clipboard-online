fc102c5123112d1faded94ba104413ea51dcbcf1
samoleu <samuel.ferralves@gmail.com>
2025-05-14 23:39:00 -0300

    feat: remove typeorm and add mongoose

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ClipboardModule } from './clipboard/clipboard.module';
-import { DatabaseModule } from './database/database.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ClipboardModule, DatabaseModule],
+  imports: [
+    ClipboardModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://samuelferralves:FV3S06CLVTNVO1bg@cluster0.zy3o1bt.mongodb.net/?retryWrites=true&w=majority',
+      // process.env.MONGODB_URI ??
+      //   (() => {
+      //     throw new Error('MONGODB_URI is not defined');
+      //   })(),
+    ),
+  ],
   controllers: [],
   providers: [],
 })
--- api/src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseProvider } from './database.provider';
-
-@Module({
-  exports: [DatabaseProvider],
-  providers: [DatabaseProvider],
-})
-export class DatabaseModule {}
